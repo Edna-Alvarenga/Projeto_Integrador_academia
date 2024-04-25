efef9daae07c03ba136209fc9150d8b69885db88
Edna-Alvarenga <ednadealvarenga@gmail.com>
2024-04-24 23:36:32 -0300

    Initial commit

---
+++ myproject/myapp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import user_login,dados_tabela,register_user,insert_update_agenda,redirect_user,update_agenda_all,update_status,dados_sair
+
+urlpatterns = [
+    path('login_usuarios/', user_login, name='login'),
+   # path('profile/', profile, name='profile'),
+    path('agenda_eventos/', dados_tabela, name='dados_tabela'),
+    path('cadastro_usuario/', register_user, name='register'),
+    path('login_usuarios/', dados_sair, name='dados_sair'),
+    path('agenda/', insert_update_agenda, name='insert_update_agenda'),
+    path('redirect-user/', redirect_user, name='redirect_user'),
+   # path('atualizar_agenda/<int:pk>/', update_agenda, name='update_agenda'),
+    path('atualizar_agenda/', update_agenda_all, name='update_agenda_all'),
+    path('atualizar_status_agenda/<int:pk>/', update_status, name='update_status'),
+  
+]
+
